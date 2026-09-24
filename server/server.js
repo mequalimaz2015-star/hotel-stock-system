c46e8c1dd15bad4e46749607b7ca45b66ee7fb6e
mequalimaz2015-star <mequalimaz2015@gmail.com>
2026-09-24 15:36:58 +0300

    Configure for single Vercel deployment

--- server/server.js
+++ server/server.js
@@ -29,4 +29,8 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
